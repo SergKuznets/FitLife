267b6e1bdb65a142d559dfb9d565a092809edddb
Сергей <kruggok@icloud.com>
2026-07-06 21:24:12 +0300

    финальная версия моего кода

--- fit_life.py
+++ fit_life.py
@@ -4,6 +4,7 @@ WATER_PER_L = 1000
 # Шаг1-Знакомство с пользователем и получение от него данных
 print('Добро пожаловать в фитнес-трекер FitLife')
 user_name = input("Введите ваше имя:")
+user_name = user_name.title()
 user_age = int(input('Сколько Вам лет:'))
 # Шаг2-Запрос веса в кг и роста в метрах
 user_weight = float(input('Введите свой вес в кг, используйте точку:'))
@@ -16,7 +17,7 @@ water_ml = user_weight * WATER_PER_ML
 water_l = round((water_ml / WATER_PER_L), 2)
 print()
 # Шаг3-красиво оформляем
-print(f"Отчет для пользователя: {user_name.title()} ({user_age} г.)")
+print(f"Отчет для пользователя: {user_name} ({user_age} г.)")
 print(f"Твой индекс массы тела: {bmi}")
 print(f"Рекомендуемая норма воды: {water_l} л. в день")
 print()
